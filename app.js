46f1b86520eddf322acd9ab4f96c3bcbaec30ae2
NaxxOre <helixzz86@gmail.com>
2026-01-12 01:42:42 +0630

    docs: clarify usage

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-const o=document.getElementById('o');document.getElementById('b').onclick=()=>{const d=new Date();o.textContent=`Commit 15\nISO: ${d.toISOString()}\nLucky: ${Math.floor(Math.random()*999999)}`;};
+const o=document.getElementById('o');document.getElementById('b').onclick=()=>{const d=new Date();o.textContent=`Commit 16\nISO: ${d.toISOString()}\nLucky: ${Math.floor(Math.random()*999999)}`;};
